fb5150035e3cccff0090ddd457e1743236cbbf51
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-06-14 21:50:24 +0300

    pipeline for converting tokens to DataSet

---
+++ yasearch/processing/__init__.py
@@ -0,0 +1,6 @@
+from yasearch.processing.silo import igniset
+from yasearch.processing.tokenizer import ITokenizer
+from yasearch.processing.mask import IProcessor
+
+
+__all__ = ["igniset", "ITokenizer", "IProcessor"]
